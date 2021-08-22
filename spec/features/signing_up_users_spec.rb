4ffc4d51eebb194631301ac659aff25f1dc43da1
Anna Moelling <anna.moelling@googlemail.com>
2021-08-22 18:24:33 +0200

    user signup

---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "user signup" do
+  scenario "with valid credentials" do
+    visit "/"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_button "Sign up"
+    expect(page).to have_content("You have signed up successfully.")
+  end
+
+  scenario 'with invalid credentials' do
+    visit "/" 
+    click_link "Sign up"
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+  end
+
+end
