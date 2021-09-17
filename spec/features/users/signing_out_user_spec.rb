e7ab3accf87029b443c57488b5fb616444a68337
Anna Moelling <anna.moelling@googlemail.com>
2021-09-17 17:23:30 +0200

    add first name and last name to sign up form

--- spec/features/users/signing_out_user_spec.rb
+++ spec/features/users/signing_out_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "user sign out" do
   before do
-    @vitor = User.create(email: "vitor@web.br", password: "123456")
+    @vitor = User.create(email: "vitor@web.br", password: "123456", first_name: "Vitor", last_name: "Reis")
     visit "/"
     click_link "Sign in"
     fill_in "Email", with: @vitor.email
@@ -11,7 +11,6 @@ RSpec.describe "user sign out" do
   end
 
   scenario do
-    visit "/"
     click_link "Sign out"
     expect(page).to have_content("Signed out successfully.")
     expect(page).to have_link("Sign up")
--- spec/features/users/signing_up_users_spec.rb
+++ spec/features/users/signing_up_users_spec.rb
@@ -4,20 +4,31 @@ RSpec.describe "user signup" do
   scenario "with valid credentials" do
     visit "/" 
     click_link "Sign up"
+    fill_in "First name", with: "Vitor"
+    fill_in "Last name", with: "Reis"
     fill_in "Email", with: "vitor@web.br"
     fill_in "Password", with: "123456"
     fill_in "Password confirmation", with: "123456"
     click_button "Sign up"
     expect(page).to have_content("You have signed up successfully.")
+    
+    visit "/"
+    expect(page).to have_content("Vitor Reis")
   end
 
-  scenario 'with invalid credentials' do
-    visit "/" 
+  scenario "with invalid credentials" do
+    visit "/"
+
     click_link "Sign up"
-    fill_in "Email", with: ""
-    fill_in "Password", with: ""
-    fill_in "Password confirmation", with: ""
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password",  with: "password"
+    fill_in "Password confirmation",  with: "password"
     click_button "Sign up"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
   end
 
 end
